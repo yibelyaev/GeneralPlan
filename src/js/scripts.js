c743390b803e33e94ebb7c7bf71f2e62b671c4c6
Yaroslav <yibelyaev@icloud.com>
2023-06-03 18:43:03 +0300

    save

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 const menuButton = document.querySelector('.menu__button');
 const nav = document.querySelector('.menu')
 
+
 menuButton.addEventListener('click', () => {
   let expanded = menuButton.getAttribute('aria-expanded') === 'true';
   menuButton.setAttribute('aria-expanded', !expanded);
