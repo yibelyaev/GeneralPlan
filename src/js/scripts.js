c463b2265f619c10712e308835e789c0abef40cd
Yaroslav <yibelyaev@icloud.com>
2023-06-05 20:45:58 +0300

    update popover

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,3 +7,19 @@ window.addEventListener('DOMContentLoaded', () => {
     initModals();
   });
 });
+
+
+const anchors = document.querySelectorAll('a.[href="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href')
+
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
